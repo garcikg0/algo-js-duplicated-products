57d5539b8b57aeb13d8a5aa6ffff7d8a8982be15
garcikg0 <kg.gar91@gmail.com>
2021-01-06 22:29:08 -0500

    type node index in terminal to run

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-nameStr1 = ['ball', 'bat', 'glove', 'glove', 'glove'];
-priceStr1 = [2,3,1,2,1]
-weightStr1 = [2,5,1,1,1]
+const nameStr1 = ['ball', 'bat', 'glove', 'glove', 'glove'];
+const priceStr1 = [2,3,1,2,1]
+const weightStr1 = [2,5,1,1,1]
 
 // nameStr = ['ball', 'box', 'lamp', 'brick', 'pump'];
 // priceStr = [2,2,2,2,2]
 // weightStr = [2,2,2,2,2]
 
 
-function numDuplicates(nameStr, priceStr, weightStr){
+function numDuplicates(nameStr, priceStr, weightStr) {
     console.log(nameStr)
     console.log(priceStr) 
     console.log(weightStr)
 };
 
+numDuplicates(nameStr1, priceStr1, weightStr1);
