d7a6ec12e1dff722f6562ef4a53387d4fe992380
garcikg0 <kg.gar91@gmail.com>
2021-01-06 22:16:30 -0500

    trying to run algo with no success

---
+++ index.js
@@ -0,0 +1,15 @@
+nameStr1 = ['ball', 'bat', 'glove', 'glove', 'glove'];
+priceStr1 = [2,3,1,2,1]
+weightStr1 = [2,5,1,1,1]
+
+// nameStr = ['ball', 'box', 'lamp', 'brick', 'pump'];
+// priceStr = [2,2,2,2,2]
+// weightStr = [2,2,2,2,2]
+
+
+function numDuplicates(nameStr, priceStr, weightStr){
+    console.log(nameStr)
+    console.log(priceStr) 
+    console.log(weightStr)
+};
+
