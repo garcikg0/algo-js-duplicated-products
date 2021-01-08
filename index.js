710851f913134f7283ebc0120f84552f9bd0061d
garcikg0 <kg.gar91@gmail.com>
2021-01-07 21:54:08 -0500

    Linked List started

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const nameStr1 = ['ball', 'bat', 'glove', 'glove', 'glove'];
 const priceStr1 = [2,3,1,2,1]
 const weightStr1 = [2,5,1,1,1]
 
-// nameStr = ['ball', 'box', 'lamp', 'brick', 'pump'];
-// priceStr = [2,2,2,2,2]
-// weightStr = [2,2,2,2,2]
+// const nameStr2 = ['ball', 'box', 'lamp', 'brick', 'pump'];
+// const priceStr2 = [2,2,2,2,2]
+// const weightStr2 = [2,2,2,2,2]
 
 
 function numDuplicates(nameStr, priceStr, weightStr) {
